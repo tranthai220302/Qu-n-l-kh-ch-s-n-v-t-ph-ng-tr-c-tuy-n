d7f094f7716c0c644db8683ffb96ab9a04168d92
Tran Quang THAI <tranthai220203@gmail.com>
2024-03-13 09:43:12 +0700

    edit newRequest

--- src/ults/newRequest.js
+++ src/ults/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://booking-5z8s.onrender.com/api",
   withCredentials: true,
 });
 
