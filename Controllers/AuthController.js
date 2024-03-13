f67f8c11aa947b458d0d5762c2d360cc72668b35
Trần Quang Thái <103127061+tranthai220302@users.noreply.github.com>
2024-03-13 10:43:49 +0700

    Update AuthController.js

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -13,7 +13,7 @@ export const loginController = async(req, res, next) =>{
             httpOnly: true,
             secure: true,
             sameSite: 'None',
-            domain: 'www.booking-5z8s.onrender.com',
+            domain: 'www.harumi.site',
             path: "/",
         }).status(200).send(token.user)
     } catch (error) {
