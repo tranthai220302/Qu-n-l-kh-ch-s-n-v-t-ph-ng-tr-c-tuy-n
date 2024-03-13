115c40b420ba1e31a2601813ab94626c87d8df72
Trần Quang Thái <103127061+tranthai220302@users.noreply.github.com>
2024-03-13 10:46:05 +0700

    Update newRequest.js

--- src/ults/newRequest.js
+++ src/ults/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://booking-5z8s.onrender.com/api",
+  baseURL: "https://www.harumi.site/api",
   withCredentials: true,
 });
 
